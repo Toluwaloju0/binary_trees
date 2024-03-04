c5704364cd8576faa6bf653c76a5708f8660c14b
Toluwaloju0 <teecoded1990@gmail.com>
2024-03-04 21:22:41 +0100

    binary_tree

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -16,8 +16,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 		return (0);
 	}
 
-	a = 1 + binary_tree_size(tree->left);
-	a = 1 + binary_tree_size(tree->right);
+	a = 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
 
 	return (a);
 }
